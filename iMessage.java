ec627adb1b0b9a5f4c460ab53a681757e2b72219
Kamal Gurung <92692267+Kamalgrg300@users.noreply.github.com>
2023-02-08 20:26:04 -0500

    Update iMessage.java

--- iMessage.java
+++ iMessage.java
@@ -35,6 +35,6 @@ class Message{
         System.out.println("registerNumber: "+ referenceNumber +  "   item: "+item);
     }
     void image(long item, byte registerNumber){
-        System.out.println("" item : "+item + "    registerNumber: "+ registerNumber);
+        System.out.println(" item : "+item + "    registerNumber: "+ registerNumber);
     }
 }
